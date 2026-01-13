8adc776d9aa62ecffa79d7eb5df50c797a0a83c0
Atul Thakur <atulthakur639499@gmail.com>
2026-01-13 11:51:59 +0530

    Added loop for block

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const blockWidth = 30;
 const cols = Math.floor(board.clientWidth / blockWidth );
 const rows = Math.floor(board.clientHeight / blockHeight );
 
+for(let i=0;i<rows*cols;i++){
+    const block = document.createElement('div');
+    board.classList.add("block")
+    board.appendChild(block);
+}
